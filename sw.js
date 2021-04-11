c3228e48cb9f0b7e1bc52a68e43304756534e0f5
Ant0ineGC <a.gaudetchardonnet@gmail.com>
2021-04-11 08:32:41 +0200

    !online

--- sw.js
+++ sw.js
@@ -1,11 +1,17 @@
-this.addEventListener('install', function(event) { 
-    event.waitUntil( 
-        caches.open('v1').then(function(cache) { 
-          return cache.addAll([ 
-              '/index.html', 
-              '/style.css', 
-              '/app.js', 
-          ]); 
-        }) 
-    ); 
-}); 
+self.addEventListener('install', (evt) => {
+    console.log(`sw installé à ${new Date().toLocaleTimeString()}`);
+});
+
+self.addEventListener('activate', (evt) => {
+    console.log(`sw activé à ${new Date().toLocaleTimeString()}`);    
+});
+
+self.addEventListener('fetch', (evt) => {
+    if(!navigator.onLine) {
+        const headers = { headers: { 'Content-Type': 'text/html;charset=utf-8'} };
+        evt.respondWith(new Response('<h1>Pas de connexion internet</h1><div>Apllication en mode dégradé. Veuillez vous connecter</div>', headers));
+    }
+    
+    console.log('sw intercepte la requête suivante via fetch', evt);
+    console.log('url interceptée', evt.request.url);
+});
