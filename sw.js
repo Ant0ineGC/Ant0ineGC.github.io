21acb49842973214161ed592d521137213c5ab17
Ant0ineGC <a.gaudetchardonnet@gmail.com>
2021-04-17 01:47:45 +0200

    network first strategy

--- sw.js
+++ sw.js
@@ -19,25 +19,16 @@ self.addEventListener('activate', (evt) => {
     console.log(`sw activé à ${new Date().toLocaleTimeString()}`);    
 });
 
-self.addEventListener('fetch', (evt) => {
-    if(!navigator.onLine) {
-        const headers = { headers: { 'Content-Type': 'text/html;charset=utf-8'} };
-        evt.respondWith(new Response('<h1>Pas de connexion internet</h1><div>Apllication en mode dégradé. Veuillez vous connecter</div>', headers));
-    }
-    
-    console.log('sw intercepte la requête suivante via fetch', evt);
-    console.log('url interceptée', evt.request.url);
-
+// network first strategy
+self.addEventListener('fetch', evt => {
     evt.respondWith(
-        caches.match(evt.request).then (res => {
-            if (res) {
-                return (res);
-            }
-            return fetch (evt.request).then(newResponse => {
-                caches.open (cacheNAme).then (cache => cache.put (evt.request, newResponse));
-                return newResponse.clone();
-            })
+        fetch(evt.request).then( res => {
+            // we add the latest version into the cache
+            caches.open(cacheName).then(cache => cache.put(evt.request, res));
+            // we clone it as a response can be read only once (it's like a one time read stream)
+            return res.clone();
         })
+        .catch(err => caches.match(evt.request))
     );
 });
 
