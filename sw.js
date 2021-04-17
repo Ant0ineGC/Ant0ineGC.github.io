54ed391f046d7aaa0cff477c1354bbb0c8992879
Ant0ineGC <a.gaudetchardonnet@gmail.com>
2021-04-17 02:36:41 +0200

    a

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@ const cacheNAme = 'veille-techno-2.0'
 
 self.addEventListener('install', (evt) => {
     console.log(`sw installé à ${new Date().toLocaleTimeString()}`);
-    caches.open(cacheNAme).then(cache => {
-        cache.addAll([
+    const cachePromise = caches.open(cacheNAme).then(cache => {
+        return cache.addAll([
             'index.html',
             'style.css',
             'vendors/bootsrap4.min.css',
@@ -12,7 +12,8 @@ self.addEventListener('install', (evt) => {
             'contact.html',
             'contact.js',
         ])
-    })
+    });
+    evt.waitUntil(cachePromise);
 });
 
 self.addEventListener('activate', (evt) => {
@@ -23,12 +24,16 @@ self.addEventListener('activate', (evt) => {
 self.addEventListener('fetch', evt => {
     evt.respondWith(
         fetch(evt.request).then( res => {
+            console.log(`${evt.request.url} fétché depuis depuis le reseau`);
             // we add the latest version into the cache
             caches.open(cacheName).then(cache => cache.put(evt.request, res));
             // we clone it as a response can be read only once (it's like a one time read stream)
             return res.clone();
         })
-        .catch(err => caches.match(evt.request))
+        .catch(err => {
+            console.log(`${evt.request.url} fétché depuis depuis le cache`);
+            caches.match(evt.request);
+        })
     );
 });
 
